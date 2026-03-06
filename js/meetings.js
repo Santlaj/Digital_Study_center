a457beb69e97e51e6f096f53c1c67c2f513c4fbc
Santlaj Kumar Mehta <santlaj2006@gmail.com>
2026-03-06 21:29:00 +0530

    Delete js directory

--- js/meetings.js
+++
@@ -1,8 +0,0 @@
-async function saveMeeting(classNum, title, meetLink, dateTime) {
-  await db.collection("meetings").add({
-    class: classNum,
-    title,
-    meetLink,
-    dateTime
-  });
-}
--- js/notes.js
+++
@@ -1,9 +0,0 @@
-async function saveNoteMeta(classNum, subject, fileLink, fileName) {
-  await db.collection("notes").add({
-    class: classNum,
-    subject,
-    fileName,
-    driveLink: fileLink,
-    uploadedAt: firebase.firestore.FieldValue.serverTimestamp()
-  });
-}
